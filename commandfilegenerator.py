5cb6052b930363e637ad4415cd381098af6bc723
Richard Wattenbarger <github@wattenbarger.net>
2023-01-23 18:27:34 -0500

    initial commit

---
+++ commandfilegenerator.py
@@ -0,0 +1,67 @@
+from argparse import ArgumentParser
+from collections.abc import Iterable
+from functools import partial
+from pathlib import Path
+import dataclasses
+import csv
+
+@dataclasses.dataclass
+class Excerpt():
+    start: float
+    end: float
+    label: str
+    duration: float = dataclasses.field(init=False)
+    
+    def __post_init__(self):
+        self.duration = self.end - self.start
+        
+def read_label_file(label_file: Path) -> Iterable[tuple[str, str, str]]:
+    with label_file.open() as fp:
+        return [tuple(row) for row in csv.reader(fp, dialect='excel-tab')]
+    
+def row_to_excerpt(row):
+    start_str, end_str, label = row
+    return Excerpt(float(start_str), float(end_str), label)
+    
+def make_ffmpeg_command(excerpt, filename=None):
+    FILENAME = '"filename"'
+    return f'ffmpeg -i "{filename if filename else FILENAME}" ' \
+           f'-ss {excerpt.start:.3f} -t {excerpt.duration:.3f} -vn ' \
+           f'-acodec copy "{excerpt.label}.mp4"'
+    
+def write_ffmpeg_command_file(output_file: Path, ffmpeg_commands): 
+    with output_file.open(mode='w') as fp:
+        for ffmpeg_command in ffmpeg_commands:
+            print(ffmpeg_command, file=fp)
+    
+def generate_script_file(label_file: Path, media_file: Path, output_file: Path):
+    rows = read_label_file(label_file)
+    excerpts = [row_to_excerpt(row) for row in rows]
+    make_ffmpeg_command_2 = \
+        partial(make_ffmpeg_command, filename=media_file)
+
+    ffmpeg_commands = [make_ffmpeg_command_2(excerpt) for excerpt in excerpts]
+
+    write_ffmpeg_command_file(output_file, ffmpeg_commands)
+    
+def setup_command_line():
+    parser = ArgumentParser()
+    parser.add_argument('media_file', type=Path)
+    parser.add_argument('label_file', type=Path)
+    parser.add_argument('output_file', type=Path)
+    return parser.parse_args()
+
+def main():
+    command_line_args = setup_command_line()
+    rows = read_label_file(command_line_args.label_file)
+    excerpts = [row_to_excerpt(row) for row in rows]
+    make_ffmpeg_command_2 = \
+        partial(make_ffmpeg_command, filename=command_line_args.media_file)
+
+    ffmpeg_commands = [make_ffmpeg_command_2(excerpt) for excerpt in excerpts]
+
+    output_file = command_line_args.output_file
+    write_ffmpeg_command_file(output_file, ffmpeg_commands)
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,132 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+from pathlib import Path
+from tkinter import filedialog
+
+from commandfilegenerator import generate_script_file
+
+class FileSelectionUI():
+
+    def __init__(self):
+       self._setup()
+
+    def _setup(self):
+        
+        self.window = tk.Tk()
+        self.window.title('Parse labels')
+        self.window.resizable(width=True, height=False)
+
+        self.var_labels_file = tk.StringVar()
+        self.var_media_file = tk.StringVar()
+        self.var_script_file = tk.StringVar()
+
+        top_frame = ttk.Frame(master=self.window, padding=(5, 5, 5, 0))
+        top_frame.columnconfigure(index=0, weight=0)
+        top_frame.columnconfigure(index=1, weight=1, minsize=120)
+        top_frame.columnconfigure(index=2, weight=0)
+        top_frame.rowconfigure(index=0, weight=0)
+        top_frame.rowconfigure(index=1, weight=0)
+        top_frame.rowconfigure(index=2, weight=0)
+
+        lbl_labels_file = ttk.Label(master=top_frame, text='Labels file:')
+        lbl_labels_file.grid(row=0, column=0, sticky='nw')
+
+        self.ent_labels_file = ttk.Entry(master=top_frame, 
+            textvariable=self.var_labels_file)
+        self.ent_labels_file.grid(row=0, column=1, columnspan=2, sticky='new')
+
+        btn_browse_labels_file = ttk.Button(master=top_frame, text='Browse ...', 
+            command=self.browse_for_labels_file)
+        btn_browse_labels_file.grid(row=0, column=3, sticky='ne')
+
+        lbl_media_file = ttk.Label(master=top_frame, text='Media file:')
+        lbl_media_file.grid(row=1, column=0, sticky='nw')
+
+        self.ent_media_file = ttk.Entry(master=top_frame,
+            textvariable=self.var_media_file)
+        self.ent_media_file.grid(row=1, column=1, columnspan=2, sticky='new')
+
+        btn_browse_media_file = ttk.Button(master=top_frame, text='Browse ...',
+            command=self.browse_for_media_file)
+        btn_browse_media_file.grid(row=1, column=3, sticky='ne')
+
+        lbl_script_file = ttk.Label(master=top_frame, text='Script file:')
+        lbl_script_file.grid(row=2, column=0, sticky='nw')
+
+        self.ent_script_file = ttk.Entry(master=top_frame,
+            textvariable=self.var_script_file)
+        self.ent_script_file.grid(row=2, column=1, columnspan=2, sticky='new')
+
+        btn_browse_script_file = ttk.Button(master=top_frame, text='Browse ...',
+            command=self.browse_for_script_file)
+        btn_browse_script_file.grid(row=2, column=3, sticky='ne')
+
+        top_frame.pack(fill=tk.X)
+        
+        bottom_frame = ttk.Frame(master=self.window, padding=(5,0,5,5))
+
+        btn_ok = ttk.Button(master=bottom_frame, text="OK",
+            command=self.create_ffmpeg_script)
+        btn_ok.pack(side=tk.RIGHT)
+        
+        btn_cancel = ttk.Button(master=bottom_frame, text="Cancel",
+            command=self.window.destroy)
+        btn_cancel.pack(side=tk.RIGHT)
+
+        bottom_frame.pack(fill=tk.X)
+        
+    def browse_for_labels_file(self):
+        filename = filedialog.askopenfilename(
+                        title='Select labels file',
+                        filetypes=[('text files', '*.txt'),
+                                   ('All files', '*.*')],
+                        defaultextension='.txt')
+        if filename:
+            self.var_labels_file.set(filename)
+        
+    def browse_for_media_file(self):
+        filename = filedialog.askopenfilename(
+            title='Select media file',
+            filetypes=[('MP4 files', '*.mp4'),
+                       ('M4A files', '*.m4a'),
+                       ('MP3 files', '*.mp3'),
+                       ('All files', '*.*')],
+                       defaultextension='.mp4')
+        if filename:
+            self.var_media_file.set(filename)
+        
+    def browse_for_script_file(self):
+        filename = filedialog.asksaveasfilename(
+                    title='Save script as',
+                    defaultextension='.sh',
+                    filetypes=[('bash scripts', '*.sh'),
+                               ('All files', '*.*')])
+        if filename:
+            self.var_script_file.set(filename)
+            
+    def create_ffmpeg_script(self):
+        labels_file = Path(self.var_labels_file.get())
+        media_file = Path(self.var_media_file.get())
+        script_file = Path(self.var_script_file.get())
+        generate_script_file(labels_file, media_file, script_file)        
+
+        
+def main():
+    ui = FileSelectionUI()
+
+    width = 400
+    height = 135
+
+    screen_width = ui.window.winfo_screenwidth()
+    screen_height = ui.window.winfo_screenheight()
+
+    left = (screen_width - width) // 2 
+    top = (screen_height - height) // 2
+
+    ui.window.geometry(f'{width}x{height}+{left}+{top}')
+
+    ui.window.mainloop()
+    
+if __name__ == '__main__':
+    main()
